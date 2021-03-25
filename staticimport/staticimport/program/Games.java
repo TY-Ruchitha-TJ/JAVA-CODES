cdeb73072c5125209dbd648a513a52686faff505
Ruchitha T J <ruchitha.tj@testyantra.in>
2021-03-25 18:22:34 +0530

    staticimport/program/Games.java

---
+++ staticimport/staticimport/program/Games.java
@@ -0,0 +1,11 @@
+package com.tyss.staticimport.program;
+
+public class Games {
+	 public static void work() {
+		System.out.println("working");
+	}
+	public static void play() {
+		System.out.println("playing");
+	}
+
+}
---
+++ staticimport/staticimporting/pro/TestGames.java
@@ -0,0 +1,11 @@
+package com.tyss.staticimporting.pro;
+
+import com.tyss.staticimport.program.Games;
+
+public class TestGames {
+public static void main(String[] args) {
+
+ Games.play();
+ Games.work();
+}
+}
